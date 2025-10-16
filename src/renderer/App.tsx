0d8feb219948cf393e2c22d427ba7a0862a6cfb5
Muhammed Adhil p <167611139+adhilbathali@users.noreply.github.com>
2025-10-16 12:46:37 +0530

    change react setup
    add main.tsx for convenience

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css'
 import TodoBox from './components/todo';
 import electron from './asset/electron-icon.svg';
 import reactJs from './asset/react-js-icon.svg';
 import tailwindCss from './asset/tailwind-css-icon.svg';
 import shadcnUi from './asset/shadcn-ui-icon.svg';
 import sqlite from './asset/sqlite-icon.svg';
+import './App.css'
 
-const icons = [electron, reactJs, tailwindCss, shadcnUi, sqlite];
 
+function App() {
+  const icons = [electron, reactJs, tailwindCss, shadcnUi, sqlite];
 
-const root = createRoot(document.body);
-root.render(
-    <main className="min-h-screen flex flex-col gap-10 items-center justify-center">
+  return (
+    <>
+      <main className="min-h-screen flex flex-col gap-10 items-center justify-center">
         <div className="flex gap-4">
             {icons.map((src, index) => (
                 <img key={index} src={src} className="w-15 h-15" />
@@ -21,4 +20,8 @@ root.render(
         </div>
         <TodoBox/>
     </main>
-);
+    </>
+  )
+}
+
+export default App
